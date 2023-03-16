9cee4c2be2ca7213cfe86b6a7318a5c91bd79942
Adham Allam <adham32003200@gmail.com>
2023-03-16 11:16:53 -0700

    modify return statement in 101-qoute.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,11 +6,11 @@
  * Description: A C pogram that prints exactly and that piece of art is useful"
  * - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
  *
- * Return: Always 0(Success)
+ * Return: Always 1(Fail)
 */
 int main(void)
 {
 char qoute[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(1, qoute, sizeof(qoute) / sizeof(char));
-return (0);
+return (1);
 }
