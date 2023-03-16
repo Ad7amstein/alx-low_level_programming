30b0a109de71c5b449f4c25c86a4668c1adb2ff5
Adham Allam <adham32003200@gmail.com>
2023-03-16 11:27:19 -0700

    modify style of 101-qoute.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,6 +11,6 @@
 int main(void)
 {
 char qoute[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
- write(1, qoute, (sizeof(qoute) / sizeof(char)) - 1);
+write(1, qoute, (sizeof(qoute) / sizeof(char)) - 1);
 return (1);
 }
