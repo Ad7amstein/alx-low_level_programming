2200c3d5496d8ebd833af7647d8d50636847b79a
Adham Allam <adham32003200@gmail.com>
2023-03-16 11:14:08 -0700

    Print using write

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ * main - Entry poin
+ *
+ * Description: A C pogram that prints exactly and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
+ *
+ * Return: Always 0(Success)
+*/
+int main(void)
+{
+char qoute[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(1, qoute, sizeof(qoute) / sizeof(char));
+return (0);
+}
