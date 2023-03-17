c13c4e80af927c6e31ce41f7374e0d745e3a192b
3llam <adham32003200@gmail.com>
2023-03-17 23:06:44 +0200

    print lphabet lower & upper

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,7 +2,7 @@
 /**
  * main - Entry point
  *
- * Description: A C program that  prints the alphabet 
+ * Description: A C program that  prints the alphabet
  * in lowercase & uppercase
  *
  * Return: int - 0(Success)
@@ -12,11 +12,11 @@ int main(void)
 int i;
 for (i = 0; i < 26; ++i)
 {
-putchar(i+97);
+putchar(i + 97);
 }
 for (i = 0; i < 26; ++i)
 {
-putchar(i+65);
+putchar(i + 65);
 }
 putchar('\n');
 return (0);
