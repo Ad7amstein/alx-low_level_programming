d7fc7c78b6725db317f1a0c309abcfac2cf68980
3llam <adham32003200@gmail.com>
2023-03-17 23:22:53 +0200

    print numbers with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C program that
+ *
+ * Return: int - 0(Success)
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar(i + 48);
+}
+putchar('\n');
+return (0);
+}
