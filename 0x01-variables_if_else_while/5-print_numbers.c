14a0980de7b2e6248d6ee86626bbd849132b0c36
3llam <adham32003200@gmail.com>
2023-03-17 23:16:30 +0200

    print digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C program that  prints the alphabet in lowercase
+ * except 'q' & 'e'
+ *
+ * Return: int - 0(Success)
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+printf("%i", i);
+}
+putchar('\n');
+}
