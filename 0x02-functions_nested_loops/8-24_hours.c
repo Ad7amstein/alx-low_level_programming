3e3a850baae91da63094ec3032dbdf8add57659a
3llam <adham32003200@gmail.com>
2023-03-22 23:07:08 +0200

    print every minute startin from 00:00 to 23:59

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,8 +16,10 @@ while (minute < 60)
 {
 _putchar((hour / 10) + '0');
 _putchar((hour % 10) + '0');
+_putchar(':');
 _putchar((minute / 10) + '0');
 _putchar((minute % 10) + '0');
+_putchar('\n');
 minute++;
 }
 hour++;
