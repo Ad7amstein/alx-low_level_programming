1a34ff10a1be10fadc702ed009043a368db5304e
3llam <adham32003200@gmail.com>
2023-03-22 13:00:05 +0200

    print the alphabet in lower case 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- * print_alphabet - program that prints the alphabet in lowercase
+ * print_alphabet_x10 - program that prints the alphabet in lowercase 10x
  */
 void print_alphabet_x10(void)
 {
 int i, j;
 j = 10;
-while(j--){
+while(j--)
+{
 for (i = 'a'; i <= 'z'; ++i)
 _putchar(i);
 
