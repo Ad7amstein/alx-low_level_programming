172719338339b3a4abf36240c901a67462b79aa1
3llam <adham32003200@gmail.com>
2023-03-22 23:38:27 +0200

    print 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * times_table - print 9 times table
+ */
+
+void times_table(void)
+{
+int r, c, n;
+r = 0;
+c = 1;
+while (r < 10)
+{
+    _putchar('0' + 0);
+    while (c < 10)
+    {
+        _putchar(',');
+        _putchar(' ');
+        n = r * c;
+        if (n < 10)
+        {
+            _putchar(' ');
+            _putchar('0' + (n % 10));
+        }
+        else
+        {
+            _putchar('0' + (n / 10));
+            _putchar('0' + (n % 10));
+        }
+        c++;
+    }
+    _putchar('\n');
+    r++;
+}
+}
