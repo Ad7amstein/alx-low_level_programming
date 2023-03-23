85280c939645a6b25eb8c117cda117836addec5c
3llam <adham32003200@gmail.com>
2023-03-24 00:09:34 +0200

    multipy tow numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - function that multiplies two integers.
+ *
+ * @a: first number
+ * @b: second number
+ * Return: int
+ * the result of multiplying tow numbers
+ */
+
+int mul(int a, int b)
+{
+int res;
+res = a *b;
+return (res);
+}
