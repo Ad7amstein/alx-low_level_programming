01d263e882fa4d296ed1a2c7151e97b26628c6cc
3llam <adham32003200@gmail.com>
2023-03-24 02:36:48 +0200

    largest prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,18 +1,35 @@
 #include <stdio.h>
-#include <math.h>
+/**
+ * sqrt - function that returns the square root of a number
+ *
+ * @x: number to find the square root of
+ *
+ * Return: square root of x
+ */
+double _sqrt(double x)
+{
+double result = x;
+double precision = 0.00001;
+
+while (result - x / result > precision)
+result = (result + x / result) / 2;
+
+return result;
+}
 
 /**
- * main - Entry point
- *
- * Description: A C program that
- * finds and prints the largest prime factor of the number 612852475143,
- * followed by a new line.
- *
- * Return: int - 0(Success)
- */
+* main - Entry point
+*
+* Description: A C program that
+* finds and prints the largest prime factor of the number 612852475143,
+* followed by a new line.
+*
+* Return: int - 0(Success)
+*/
 int main(void)
 {
-long int n = 612852475143;
+long int n = 1231952;
+int res;
 int i;
 
 while (n % 2 == 0)
@@ -20,16 +37,17 @@ while (n % 2 == 0)
 n /= 2;
 }
 
-for (i = 3; i <= sqrt(n); i += 2)
+for (i = 3; i <= _sqrt(n); i += 2)
 {
 while (n % i == 0)
 {
 n /= i;
+res = i;
 }
 }
 
 if (n > 2)
-printf("%d\n", n);
-
+res = n;
+printf("%d\n", res);
 return (0);
 }
