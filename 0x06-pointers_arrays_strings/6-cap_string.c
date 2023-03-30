f798e4632c21416c35e276fef08084629bc3c278
3llam <adham32003200@gmail.com>
2023-03-30 06:22:21 +0200

    caps the words

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,17 +8,22 @@
  */
 char *cap_string(char *str)
 {
-int i;
+int i, j;
+char delimiters[] = " \t\n,;.!?\"(){}";
 i = 0;
 while (str[i])
 {
-if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
-str[i] == ',' || str[i] == ';' || str[i] == '.' ||
-str[i] == '!' || str[i] == '?' || str[i] == '"' ||
-str[i] == '(' || str[i] == ')' || str[i] == '{' ||
-str[i] == '}')
+j = 0;
+while (delimiters[j])
+{
+if (str[i] == delimiters[j])
+{
 if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 str[i + 1] = str[i + 1] - 32;
+break;
+}
+j++;
+}
 i++;
 }
 return (str);
