efbaed9cac1d56573f2b5b4a20dd3cba09152594
3llam <adham32003200@gmail.com>
2023-04-06 06:29:53 +0200

    check if palindorme

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -16,6 +16,23 @@ int check(char *s, int n, int i)
 }
 
 /**
+ * _strlen_recursion - function that returns the length of a string.
+ *
+ * @s: string to get it's length
+ * Return: int
+ */
+int _strlen_recursion(char *s)
+{
+	int i;
+
+	i = 1;
+	if (*s == '\0')
+		return (0);
+	s++;
+	return (_strlen_recursion(s) + i);
+}
+
+/**
  * is_palindrome - function that returns 1 if a string
  * is a palindrome and 0 if not.
  *
@@ -26,9 +43,7 @@ int is_palindrome(char *s)
 {
 	int i;
 
-	i = 0;
-	while (s[i])
-		i++;
+	i = _strlen_recursion(s);
 	if (i < 2)
 		return (1);
 	return (check(s, i, 0));
