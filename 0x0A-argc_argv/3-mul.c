1e70184d0b822dafd5c0473aef9c6997b94b782a
3llam <adham32003200@gmail.com>
2023-04-08 17:26:22 +0200

    fix bug in 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,16 +7,18 @@
  * @argv: command line arguments (char*)
  * Return: Always (0) int
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-	int res;
+	int res, x, y;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	res = atoi(argv[1]) * atoi(argv[2]);
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	res = x * y;
 	printf("%d\n", res);
 	return (0);
 }
