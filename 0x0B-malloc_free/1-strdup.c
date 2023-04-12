dc9236cf5fe6ab38998c8b967c744d836e29886c
3llam <adham32003200@gmail.com>
2023-04-12 15:29:32 +0200

    dublicate string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * *_strdup - function that returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter.
+ *
+ * @str: char*
+ * Return: char*
+ */
+char *_strdup(char *str)
+{
+	int size, i;
+	char* str2;
+
+	if (str == NULL)
+		return (NULL);
+
+	size = 0;
+	i = 0;
+	while (str[size])
+	{
+		size++;
+	}
+	str2 = (char *)malloc(sizeof(char) * sizeof(size));
+	if (str2 == NULL)
+		return (NULL);
+
+	while (i < size)
+	{
+		str2[i] = str[i];
+		i++;
+	}
+	return (str2);
+}
