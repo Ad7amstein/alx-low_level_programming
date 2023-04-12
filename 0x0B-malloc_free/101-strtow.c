ac53f628083349a8618ac2ba3abdb589751f0b4d
3llam <adham32003200@gmail.com>
2023-04-12 18:22:18 +0200

    split string into words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,5 +1,25 @@
 #include "main.h"
 /**
+ * _strcmp - function that compares two strings.
+ *
+ * @s1: first string
+ * @s2: second string
+ * Return: int
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	i = 0;
+	while (s1[i] == s2[i])
+	{
+		if (s1[i] == '\0')
+			return (0);
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
+
+/**
  * **strtow - function that splits a string into words.
  *
  * @str: string
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -9,5 +9,4 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
-int _strcmp(char *s1, char *s2);
 #endif /* MAIN_H */
