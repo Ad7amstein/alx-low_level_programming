f0a98563d3efbd6ff681fcfd5d8d419cb70c74c2
3llam <adham32003200@gmail.com>
2023-04-12 15:11:46 +0200

    modify style

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,13 +10,13 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
-	char* arr;
+	char *arr;
 
 	if (size == 0)
 	{
 		return (NULL);
 	}
-	arr = (char*) malloc(sizeof(char) * size);
+	arr = (char *)malloc(sizeof(char) * size);
 	if (arr == NULL)
 	{
 		return (NULL);
