b774135af2b8f1d3ca5368ed56e280b812cbde92
3llam <adham32003200@gmail.com>
2023-04-28 03:30:32 +0300

    fix typo

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,12 +10,10 @@ size_t print_list(const list_t *h)
 	size_t n_nodes;
 
 	n_nodes = 0;
-	if (!h)
-		return (0);
 	while (h)
 	{
 		if (h->str != NULL)
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		else
 			printf("[0] (nil)\n");
 		h = h->next;
