5e9e9535f67e7bd497651232e92acfed47f2b3fa
3llam <adham32003200@gmail.com>
2023-04-28 04:28:17 +0300

    free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_list - free list
+ * 
+ * @head: pointer to first node
+ */
+void free_list(list_t *head)
+{
+	list_t *curr, *next;
+
+	curr = head;
+	while (curr)
+	{
+		next = curr->next;
+		free(curr->str);
+		free(curr);
+		curr = next;
+	}
+}
