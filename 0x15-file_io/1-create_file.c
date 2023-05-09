d4cef4b52bdf82de5b3b86f77d7bd1ef617cd65c
3llam <adham32003200@gmail.com>
2023-05-09 15:51:32 +0300

    write to a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,13 +9,14 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fp;
+	ssize_t n;
 
 	if (filename == NULL)
 		return (-1);
 	fp = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (text_content)
 	{
-		ssize_t n = write(fp, text_content, strlen(text_content));
+		n = write(fp, text_content, strlen(text_content));
 		if (n == -1)
 		{
 			close(fp);
