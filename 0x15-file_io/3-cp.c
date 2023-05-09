9e3a16b1c03e231784e914196b59e746b3969142
3llam <adham32003200@gmail.com>
2023-05-09 16:29:49 +0300

    copy from file to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,14 +15,12 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
+	if ((fd_from = open(argv[1], O_RDONLY)) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
+	if ((fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664)) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
