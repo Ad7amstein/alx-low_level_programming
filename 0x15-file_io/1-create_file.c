fc87e34ac74612d9d504110558b39f5beb80938c
3llam <adham32003200@gmail.com>
2023-05-09 15:50:03 +0300

    write to a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include <main.h>
+/**
+ * create_file - function that creates a file.
+ *
+ * @filename: file
+ * @text_content: text
+ * Return: int
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fp;
+
+	if (filename == NULL)
+		return (-1);
+	fp = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (text_content)
+	{
+		ssize_t n = write(fp, text_content, strlen(text_content));
+		if (n == -1)
+		{
+			close(fp);
+			return (-1);
+		}
+	}
+	close(fp);
+	return (1);
+}
