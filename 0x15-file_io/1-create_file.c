cb4402d53704d4038ea7aef21fbc67fa2d45bbaf
3llam <adham32003200@gmail.com>
2023-07-11 00:03:18 +0300

    modify style

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,21 +8,21 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fp;
-    ssize_t n;
+	int fp;
+	ssize_t n;
 
-    if (filename == NULL)
-        return (-1);
-    fp = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (text_content)
-    {
-        n = write(fp, text_content, strlen(text_content));
-        if (n == -1)
-        {
-            close(fp);
-            return (-1);
-        }
-    }
-    close(fp);
-    return (1);
+	if (filename == NULL)
+		return (-1);
+	fp = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (text_content)
+	{
+		n = write(fp, text_content, strlen(text_content));
+		if (n == -1)
+		{
+			close(fp);
+			return (-1);
+		}
+	}
+	close(fp);
+	return (1);
 }
