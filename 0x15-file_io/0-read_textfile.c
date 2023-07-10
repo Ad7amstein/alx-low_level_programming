f2ba4c4a4dc382e0ff114d6880bb4b17be410a7a
3llam <adham32003200@gmail.com>
2023-07-10 23:31:09 +0300

    read from file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * read_textfile -  function that reads a
+ * text file and prints it to the POSIX standard output.
+ *
+ * @filename: file
+ * @letters: letters to be written
+ * Return: ssize_t
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t bytes_read, bytes_written;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = fopen(filename, O_RDONLY);
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+	bytes_read = read(fd, buf, letters);
+	if (bytes_read == -1)
+	{
+		close(fd);
+		free(buf);
+		return (0);
+	}
+	bytes_written = write(STDOUT_FILENO, buf, bytes_read);
+	if (bytes_written == -1 || bytes_read != bytes_written)
+	{
+		close(fd);
+		free(buf);
+		return (0);
+	}
+	close(fd);
+	free(buf);
+	return (bytes_written);
+}
