e675b1d1c79649d7274f73edb3f23b79b9c5775e
3llam <adham32003200@gmail.com>
2023-07-10 23:50:25 +0300

    add conversion type

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -20,7 +20,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
+	buf = (char *)malloc(sizeof(char) * letters);
 	if (buf == NULL)
 	{
 		close(fd);
