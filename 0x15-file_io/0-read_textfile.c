2f94513390d460494ea0410c6eafb11a9be2b1ea
3llam <adham32003200@gmail.com>
2023-07-10 23:48:53 +0300

    fix open file bug

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,6 +18,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 	buf = malloc(sizeof(char) * letters);
 	if (buf == NULL)
 	{
