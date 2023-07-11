ca6f479b887e9a829ddd5b23c7d75ede3f8bf7d0
3llam <adham32003200@gmail.com>
2023-07-12 00:54:43 +0300

    modify style

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,21 +16,21 @@
  */
 int main(int argc, char *argv[])
 {
-	int fp_from, fp_to;
+	int fp_from = open(argv[1], O_RDONLY),
+		fp_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	ssize_t bytes_read, bytes_written;
 	char buf[BUFFER_SIZE];
+
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit (97);
+		exit(97);
 	}
-	fp_from = open(argv[1], O_RDONLY);
 	if (fp_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fp_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fp_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[1]);
