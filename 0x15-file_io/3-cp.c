556799038bc49023dc4301727740db9f9656cfad
3llam <adham32003200@gmail.com>
2023-07-12 00:36:59 +0300

    fix open file bug

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -40,7 +40,7 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	fp_to = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	fp_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fp_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[1]);
