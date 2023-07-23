1f81204b0703ae3e597cf97963a4e243209699e8
3llam <adham32003200@gmail.com>
2023-07-23 16:22:54 +0300

    modify style

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,23 +8,23 @@
  */
 int _atoi(char *s)
 {
-int sign, j, i;
-unsigned int res;
-sign = 1;
-res = 0;
-for (i = 0; s[i] != '\0'; i++)
-{
-if (s[i] == '-')
-sign *= -1;
-if (s[i] >= '0' && s[i] <= '9')
-break;
-}
-j = i;
-while (s[j] >= '0' && s[j] <= '9')
-{
-res = (res * 10) + (s[j] - '0');
-j++;
-}
-res *= sign;
-return (res);
+	int sign, j, i;
+	unsigned int res;
+	sign = 1;
+	res = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		if (s[i] >= '0' && s[i] <= '9')
+			break;
+	}
+	j = i;
+	while (s[j] >= '0' && s[j] <= '9')
+	{
+		res = (res * 10) + (s[j] - '0');
+		j++;
+	}
+	res *= sign;
+	return (res);
 }
