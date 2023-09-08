c784fbfe80365c359f1b993b596dd1885622d435
Ad7amstein <adham32003200@gmail.com>
2023-09-08 06:34:05 -0700

    Retrive a value associated with a key

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -5,6 +5,8 @@
  *
  * @ht: the hash table
  * @key: key to be retrived
+ *
+ * Return: The value associated with the key
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
@@ -13,7 +15,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 
 	if (ht == NULL || key == NULL)
 		return (NULL);
-	
+
 	index = key_index((const unsigned char *) key, ht->size);
 	current_item = ht->array[index];
 	if (current_item == NULL)
@@ -22,7 +24,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	{
 		if (strcmp(current_item->key, key) == 0)
 			return (current_item->value);
-		
+
 		current_item = current_item->next;
 	}
 	return (NULL);
